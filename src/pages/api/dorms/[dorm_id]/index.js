7299c80af90d02b753fa15c8b98939ff5d77d310
Jackson Stone <jlstone@middlebury.edu>
2025-04-28 19:33:31 -0400

    Now fetching dorms from DB instead of hard coded endpoint

--- src/pages/api/dorms/[dorm_id]/index.js
+++ src/pages/api/dorms/[dorm_id]/index.js
@@ -1,48 +1,20 @@
-// export default async function handler(req, res) {
-//     const { dorm_id } = req.query;
-//     const dorm = await db("dorms").where({ dorm_id }).first();
+import { createRouter } from "next-connect";
+import Building from "../../../../../models/Building";
 
-//     if (!dorm) {
-//       res.status(404).json({ error: "Dorm not found" });
-//     } else {
-//       res.status(200).json(dorm);
-//     }
-//   }
+const router = createRouter();
 
-// test stuff:
-
-export default function handler(req, res) {
+router.get(async (req, res) => {
   const { dorm_id } = req.query;
-
-  const dorms = {
-    gifford: {
-      id: "Gifford",
-      name: "Gifford",
-      building_type: "Suites, Singles",
-      residents: "Sophmores",
-      mapId: "511466",
-    },
-    battell: {
-      id: "Battell",
-      name: "Battell",
-      building_type: "Singles, Doubles",
-      residents: "Freshman",
-      mapId: "511452",
-    },
-    forest: {
-      id: "Forest",
-      name: "Forest",
-      building_type: "Sink-mate singles",
-      residents: "Seniors/Febs",
-      mapId: "571679",
-    },
-  };
-
-  const dorm = dorms[dorm_id];
-
-  if (!dorm) {
-    res.status(404).json({ error: "Dorm not found" });
-  } else {
+  try {
+    const dorm = await Building.query().findById(dorm_id);
+    if (!dorm) {
+      return res.status(404).json({ error: "Dorm not found" });
+    }
     res.status(200).json(dorm);
+  } catch (error) {
+    console.error("Error fetching dorm:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-}
+});
+
+export default router.handler();
